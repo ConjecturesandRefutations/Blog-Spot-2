6d1f123f693d0d5da6cb459f7d7282d78093470e
Alfie Collins <alfiecollins44@gmail.com>
2023-05-08 11:57:22 +0100

    adding a signup page and additional responsivity

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-function Login() {
+function Signup() {
   return (
     <div className="login">
     <div className="login-card">
       <h1>Blog Spot</h1>
-      <h2>Login</h2>
+      <h2>Signup</h2>
       <h3>Enter your details</h3>
       <form className="login-form">
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
-        <p className="loginSignup">Don't have an account?</p>
-        <a href="/signup">Signup</a>
-        <button href="#" className='loginBtn'>LOGIN</button>
+        <p className="loginSignup">Already Have an Account?</p>
+        <a href="/">Login</a>
+        <button href="#" className='loginBtn'>SIGNUP</button>
       </form>
 </div>
 </div>
@@ -20,4 +20,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
